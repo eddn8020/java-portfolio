e62562f8fbd9ceb1078a8277b6c781191aca05b1
eddn8020 <36268817+eddn8020@users.noreply.github.com>
2021-01-08 04:22:47 +0000

    Create FileHandler.java

---
+++ java-input-and-output/file-input-and-output/FileHandler.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class FileHandler {
+    public static void main(String[]args){
+        try {
+            // open input stream
+            FileReader fr = new FileReader("C:\\Users\\edwar\\Documents\\java_files\\test1.txt");
+            // then use the BufferedReader class to read text from the file
+            BufferedReader br = new BufferedReader(fr);
+            String x = br.readLine(); // read each line
+            while (x!= null){
+                System.out.print(x);
+                x = br.readLine();
+            }
+            br.close();; // close buffer reader stream
+            fr.close();; // close file reader stream
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+}
