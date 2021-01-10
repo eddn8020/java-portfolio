1b35fc437f47f38976273b2e7eddf4f2f96a92d3
eddn8020 <36268817+eddn8020@users.noreply.github.com>
2021-01-10 14:40:21 +0000

    Update FileHandler.java

--- java-input-and-output/file-input-and-output/FileHandler.java
+++ java-input-and-output/file-input-and-output/FileHandler.java
@@ -2,11 +2,14 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 
+// Here is an example of simple file handling using the FileReader and the BufferedReader classes
+// to open a file and read the text that it contains by executing the readLine method
+
 public class FileHandler {
     public static void main(String[]args){
         try {
             // open input stream
-            FileReader fr = new FileReader("test.txt");
+            FileReader fr = new FileReader("C:\\Users\\edwar\\Documents\\java_files\\test1.txt");
             // then use the BufferedReader class to read text from the file
             BufferedReader br = new BufferedReader(fr);
             String x = br.readLine(); // read each line
